0bd3529882a0b29f75a6732f0c31d680620151af
Glaucia Lauton <glaucia.lauton@gmail.com>
2024-12-03 23:04:01 -0300

    feat: add oidc provider

--- output.tf
+++ output.tf
@@ -14,6 +14,6 @@ output "subnet_priv_1b" {
   value = module.eks_network.subnet_pub_1b
 }
 
-output "eks_vpc_config" {
-  value = module.eks_cluster.eks_vpc_config
+output "oidc" {
+  value = module.eks_cluster.oidc
 }
